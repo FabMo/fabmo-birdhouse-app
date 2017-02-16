779b68a4685368dca0e1ccf54dc178bafec71632
jw4rd <j@w4rd.com>
2017-02-16 01:08:20 -0600

    bird list

--- js/birdhouse.js
+++ js/birdhouse.js
@@ -1,9 +1,108 @@
 
 var birdhouse={
+	name:"default",
 	width: 5,
 	depth: 5,
 	height: 4,
 	hole: 1.2,
+	holeHeight:4,
 	dowel:0.25,
-	material: 0.38	
+	material: 0.38,
+	install: "6 - 10\'"	
 }
+
+
+var birds = [
+	{
+	name:"Carolina Wren",
+	value:2,
+	width: 4.75,
+	depth: 4.75,
+	height: 4,
+	hole: 1.5,
+	holeHeight:4,
+	dowel:0.25,
+	material: 0.25,
+	install: "6 - 10\'"	
+	},
+	{
+	name:"Blue Bird",
+	value:3,
+	width: 5.75,
+	depth: 5.75,
+	height: 7,
+	hole: 1.5,
+	holeHeight:6,
+	dowel:0.25,
+	material: 0.38,
+	install: "4 - 6\'"	
+	},
+	{
+	name:"Chickadee",
+	value:4,
+	width: 4.75,
+	depth: 4.75,
+	height: 6,
+	hole: 1.125,
+	holeHeight:6,
+	dowel:0.25,
+	material: 0.38,
+	install: "4 - 15\'"	
+	},
+	{
+	name:"Red-Headed Woodpecker",
+	value:8,
+	width: 6.75,
+	depth: 6.75,
+	height: 9,
+	hole: 2,
+	holeHeight:8,
+	dowel:0.25,
+	material: 0.38,
+	install: "10 - 20\'"	
+	},
+	{
+	name:"Flicker",
+	value:9,
+	width: 7.75,
+	depth: 7.75,
+	height: 14,
+	hole: 2.5,
+	holeHeight:14,
+	dowel:0.25,
+	material: 0.38,
+	install: "6 - 20\'"	
+	},
+	{
+	name:"Purple Martin",
+	value:13,
+	width: 6.75,
+	depth: 6.75,
+	height: 4,
+	hole: 2.25,
+	holeHeight:4,
+	dowel:0.25,
+	material: 0.38,
+	install: "6 - 10\'"	
+	},
+	{
+	name:"Screech Owl",
+	value:20,
+	width: 9,
+	depth: 9,
+	height: 9,
+	hole: 3,
+	holeHeight:9,
+	dowel:0.38,
+	material: 0.5,
+	install: "10 - 30\'"	
+	}
+]
+
+
+
+
+
+
+
+
