aa258c5f0760ba402f96d1920223a812d6557c7e
jw4rd <j@w4rd.com>
2017-03-18 16:56:51 -0400

    dxf

--- js/make.js
+++ js/make.js
@@ -42,7 +42,92 @@ function make(){
 	pt= Math.ceil(cutDepth/((tool/10)*2))  //passes
 	pd = parseFloat(cutDepth/pt) //depth of pass
 
+	if(document.getElementById('output').value=='dxf'){
 
+		dxf = "0\nSECTION\n2\nENTITIES\n999\nw4rd.com\n0\n"
+
+		space=0
+
+		for(i=0;i<polygons.length;i++){
+
+		yOffset = 0
+
+		if((i<2)){
+			xOffset = birdhouse.width/2
+			space+=parseFloat(birdhouse.width)
+		}
+		else if(i==2){
+			xOffset = birdhouse.depth/2
+			space+=parseFloat(birdhouse.width)			
+		}
+		else if(i==3){
+			xOffset = birdhouse.width/2
+			yOffset = thickness/10
+			space+=parseFloat(birdhouse.depth)
+		}
+		else if(i==4){
+			xOffset = thickness/10
+			space+=parseFloat(birdhouse.width)
+		}
+		else if(i==5){
+			xOffset = 0
+			space+= ((parseFloat(birdhouse.width)/2)*1.41)+thickness/10
+		}
+		
+		space+=1
+
+		xOffset += space
+
+		if(polygons[i].holes.length>0){
+
+			for(j=0;j<polygons[i].holes.length;j++){
+
+				for(k=0;k<polygons[i].holes[j].length;k++){
+
+					dxf+="POLYLINE\n8\n0\n70\n1\n0\n"
+					dxf+="VERTEX\n8\n0\n10\n"
+					dxf+= (xOffset+(polygons[i].holes[j][k].X/10)).toFixed(3) + "\n20\n"
+					dxf+= (yOffset+(polygons[i].holes[j][k].Y/10)).toFixed(3) + "\n0\n"
+
+				}
+
+				dxf+="VERTEX\n8\n0\n10\n"
+				dxf+= (xOffset+(polygons[i].holes[j][0].X/10)).toFixed(3) + "\n20\n"
+				dxf+= (yOffset+(polygons[i].holes[j][0].Y/10)).toFixed(3) + "\n0\n"
+
+				dxf+="SEQEND\n0\n"
+
+			}
+		}
+
+
+		dxf+="POLYLINE\n8\n0\n70\n1\n0\n"
+
+		for(j=0;j<polygons[i].curves.length;j++){
+			dxf+="VERTEX\n8\n0\n10\n"
+			dxf+= (xOffset+(polygons[i].curves[j].X/10)).toFixed(3) + "\n20\n"
+			dxf+= (yOffset+(polygons[i].curves[j].Y/10)).toFixed(3) + "\n0\n"
+		}
+
+		dxf+="VERTEX\n8\n0\n10\n"
+		dxf+= (xOffset+(polygons[i].curves[0].X/10)).toFixed(3) + "\n20\n"
+		dxf+= (yOffset+(polygons[i].curves[0].Y/10)).toFixed(3) + "\n0\n"
+
+		dxf+="SEQEND\n0\n"
+
+		}
+
+		dxf+="ENDSEC\n0\nEOF"
+
+		var link = document.getElementById("downloadLink")
+
+		link.setAttribute("href", "data:text/plain;base64," + btoa(dxf))
+		link.setAttribute("download", "birdhouse.dxf")
+		link.click()
+
+
+	}
+	else{
 	for(i=0;i<offset.length;i++){
 
 
@@ -64,12 +149,9 @@ function make(){
 			sbp+="m3\n"
 			sbp+="g4p3\n"
 		}
-		else if(document.getElementById('output').value=='dxf'){
-			dxf = "0\nSECTION\n2\nENTITIES\n999\nw4rd.com\n0\n"
-		}
 
 		yOffset = 0
-		if((i<2)||(i==2)){
+		if(i<2){
 			xOffset = birdhouse.width/2
 		}
 		else if(i==2){
@@ -130,27 +212,10 @@ function make(){
 				}
 				sbp+="g0z0.125\n"
 
-			}
-			else{
-				dxf+="POLYLINE\n8\n0\n70\n1\n0\n"
-
-				dxf+="VERTEX\n8\n0\n10\n"
-				dxf+= (xOffset+(offset[i][j][0].X/10)).toFixed(3) + "\n20\n"
-				dxf+= (yOffset+(offset[i][j][0].Y/10)).toFixed(3) + "\n0\n"
-
-				for(k=0;k<offset[i][j].length;k++){
-					dxf+="VERTEX\n8\n0\n10\n"
-					dxf+= (xOffset+(offset[i][j][k].X/10)).toFixed(3) + "\n20\n"
-					dxf+= (yOffset+(offset[i][j][k].Y/10)).toFixed(3) + "\n0\n"
-				}
-
-				dxf+="VERTEX\n8\n0\n10\n"
-				dxf+= (xOffset+(offset[i][j][0].X/10)).toFixed(3) + "\n20\n"
-				dxf+= (yOffset+(offset[i][j][0].Y/10)).toFixed(3) + "\n0\n"
-
-				dxf+="SEQEND\n0\n"
 			}			
 		}
+
+
 
 	if(document.getElementById('output').value=='sbp_hb'){
 		sbp+="JZ,0.25\n"
@@ -194,29 +259,16 @@ function make(){
 		ext = '.nc'
 	}
 	
+	fabmo.submitJob({
+		file : sbp,
+		filename : partName + ext,
+		name : partName,
+		description :  '1/8\"endmill'
+	})
 
-	if(document.getElementById('output').value!='dxf'){
-
-		fabmo.submitJob({
-			file : sbp,
-			filename : partName + ext,
-			name : partName,
-			description :  '1/8\"endmill'
-		})
-
-	}
-	else{
-
-		dxf+="ENDSEC\n0\nEOF"
-
-		var link = document.getElementById("downloadLink")
-
-		link.setAttribute("href", "data:text/plain;base64," + btoa(dxf))
-		link.setAttribute("download", partName + ".dxf")
-		link.click()
-
-	}
 	
+	}
+
 	}
 
 
